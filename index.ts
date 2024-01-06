8539e012c3101b3668733af07b15233dcd78f5f7
dgerike <dennis.gerike+github@gmail.com>
2024-01-06 13:24:59 +0100

    extending the JimpClient to accept custom images

--- index.ts
+++ index.ts
@@ -11,5 +11,6 @@ import {FlickrClient} from "./lib/flickr/FlickrClient"
     await flickrClient.downloadOriginalImage('./data/raw') // TODO the caller also has to specify to target file name (should not be decided by the lib)
 
     const jimpClient = new JimpClient()
+    await jimpClient.setImage(`./data/raw/${process.env.FLICKR_IMAGE_ID}.jpg`)
     await jimpClient.saveProcessedImage('./data/processed', `${process.env.FLICKR_IMAGE_ID}.jpg`)
 })()
--- lib/jimp/JimpClient.ts
+++ lib/jimp/JimpClient.ts
@@ -8,6 +8,15 @@ export class JimpClient {
         this.image = new Jimp(256, 256, 0xfff000ff)
     }
 
+    /**
+     * Sets or replaces the current image.
+     * The path can be relative or absolute.
+     * The path must point to a valid (and supported) image file.
+     */
+    public async setImage(path: string) {
+        this.image = await Jimp.read(path)
+    }
+
     public async saveProcessedImage(targetPath: string, targetFile: string) {
         await fs.mkdir(`${targetPath}/`, {recursive: true})
 
