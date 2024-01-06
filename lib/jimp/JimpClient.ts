57e2bd3a912303b547ab2fbc0d741a77f0ef305d
dgerike <dennis.gerike+github@gmail.com>
2024-01-06 15:28:17 +0100

    splitting the image object into a canvas and the photo -> simplifies the internal logic and makes calculations easier

--- lib/jimp/JimpClient.ts
+++ lib/jimp/JimpClient.ts
@@ -2,15 +2,17 @@ import Jimp from "jimp"
 import fs from "fs/promises"
 
 export class JimpClient {
-    private image: Jimp
-    private aspectRatio: number = 1.5 // "1.5" equals "3:2"; "1.777777" equals "16:9"
-    private originalImageWidth: number = 256
-    private originalImageHeight: number = this.originalImageWidth / this.aspectRatio
+    private canvas: Jimp
+    private aspectRatio: number = 1 // e.g.: "1.5" equals "3:2" and "1.777777" equals "16:9"
+    private canvasImageWidth: number = 256
+    private canvasImageHeight: number = this.canvasImageWidth / this.aspectRatio
+
+    private image: Jimp | undefined
 
     constructor() {
-        this.image = new Jimp(
-            this.originalImageWidth,
-            this.originalImageHeight,
+        this.canvas = new Jimp(
+            this.canvasImageWidth,
+            this.canvasImageHeight,
             0xfff000ff
         )
     }
@@ -22,30 +24,53 @@ export class JimpClient {
      */
     public async setImage(path: string) {
         this.image = await Jimp.read(path)
-        this.originalImageWidth = this.image.getWidth()
-        this.originalImageHeight = this.image.getHeight()
-        this.applyAspectRatio()
+        this.resizeCanvasToMatchImageDimensions()
     }
 
+    /**
+     * Changes the dimensions of the canvas to match the new aspect ratio,
+     * whilst taking into account the dimensions of the image.
+     */
     public setAspectRatio(ratio: number) {
         this.aspectRatio = ratio
-        this.applyAspectRatio()
+        this.resizeCanvasToMatchImageDimensions()
     }
 
+    /**
+     * Applies all filters, then saves the produced image at the given path.
+     */
     public async saveProcessedImage(targetPath: string, targetFile: string) {
+        this.applyAllFilters()
+
         await fs.mkdir(`${targetPath}/`, {recursive: true})
 
-        this.image.write(`${targetPath}/${targetFile}`)
+        this.canvas.write(`${targetPath}/${targetFile}`)
     }
 
-    private applyAspectRatio() {
-        const finalWidth = this.originalImageWidth
-        const finalHeight = this.originalImageWidth / this.aspectRatio
+    /**
+     * Increases or decreases the dimensions of the canvas to match the size of the currently selected image.
+     * The image is NOT yet rendered to the canvas, we just reserve the space for the future.
+     * The calculation considers the selected aspect ratio and make sure the image would not be accidentally cropped.
+     */
+    private resizeCanvasToMatchImageDimensions() {
+        // TODO: the current logic only works for widescreen image, portrait images will be cropped
+        if (this.image) {
+            this.canvasImageWidth = this.image.getWidth()
+            this.canvasImageHeight = this.image.getWidth() / this.aspectRatio
+            this.canvas.contain(this.canvasImageWidth, this.canvasImageHeight)
+        }
+    }
 
-        this.image.contain(
-            finalWidth,
-            finalHeight,
-            Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE
-        )
+    /**
+     * Paints the image onto the canvas.
+     */
+    private applyAllFilters() {
+        if (this.image) {
+            this.canvas.composite(this.image, 0, 0, {
+                mode: Jimp.BLEND_SOURCE_OVER,
+                opacitySource: 1,
+                opacityDest: 1,
+            })
+        }
     }
 }
