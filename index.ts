8e2d1760cc0a0f761bec6ad67b1d18c76eb4fdce
dgerike <dennis.gerike+github@gmail.com>
2024-01-08 18:36:46 +0100

    minor renaming

--- index.ts
+++ index.ts
@@ -7,11 +7,11 @@ import {FlickrClient} from "./lib/flickr/FlickrClient"
     const flickrClient = new FlickrClient(process.env.FLICKR_API_KEY)
     const flickrImageId = process.env.FLICKR_IMAGE_ID
     flickrClient.selectPhoto(flickrImageId)
-    await flickrClient.downloadOriginalImage('./data/raw', `${flickrImageId}.jpg`)
+    await flickrClient.downloadOriginalImage('./data/original', `${flickrImageId}.jpg`)
 
     // 2. resizing the photo (so it matches the postcard format) and printing some meta information on it
     const jimpClient = new JimpClient()
-    await jimpClient.setPhoto(`./data/raw/${flickrImageId}.jpg`)
+    await jimpClient.setPhoto(`./data/original/${flickrImageId}.jpg`)
     jimpClient.setAspectRatio(1.5)
     const title = await flickrClient.getImageTitle() + ' » ' + process.env.CUSTOM_TEXT + ' « ' + flickrImageId
     await jimpClient.setTextBox(title, 8)
