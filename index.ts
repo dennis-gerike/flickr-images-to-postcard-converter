8f5a8f0319e02157c80db94dda5cdab689d59851
dgerike <dennis.gerike+github@gmail.com>
2024-01-10 17:16:09 +0100

    extending the script to be able to manage album requests AND single photo requests

--- index.ts
+++ index.ts
@@ -3,20 +3,34 @@ import {JimpClient} from "./lib/jimp/JimpClient"
 import {FlickrClient} from "./lib/flickr/FlickrClient"
 
 (async function () {
-    // 1. downloading the image file from Flickr and storing it locally
     const flickrClient = new FlickrClient(process.env.FLICKR_API_KEY)
-    const flickrImageId = process.env.FLICKR_IMAGE_ID
-    flickrClient.selectPhoto(flickrImageId)
-    await flickrClient.downloadOriginalImage('./data/original', `${flickrImageId}.jpg`)
 
-    // 2. resizing the photo (so it matches the postcard format) and printing some meta information on it
+    // 1. collecting the list of images that should be processed
+    let flickrAlbumId = 'uncategorized'
+    let flickrAlbumImageIds: string[] = []
+
+    if (process.env.FLICKR_IMAGE_ID) {
+        let flickrImageId = process.env.FLICKR_IMAGE_ID
+        flickrAlbumImageIds = [flickrImageId]
+    } else if (process.env.FLICKR_ALBUM_ID) {
+        flickrAlbumId = process.env.FLICKR_ALBUM_ID
+        flickrAlbumImageIds = await flickrClient.getAlbumImageIds(flickrAlbumId)
+    }
+
+    // 2. downloading the images from flickr and storing them locally
+    for (const photoId of flickrAlbumImageIds) {
+        await flickrClient.downloadOriginalImage(photoId, `./data/original/${flickrAlbumId}`, `${photoId}.jpg`)
+    }
+
+    // 3. resizing the photos to match the postcard format, then adding a label and finally a nice margin
     const jimpClient = new JimpClient()
-    await jimpClient.setPhoto(`./data/original/${flickrImageId}.jpg`)
-    jimpClient.setAspectRatio(1.5)
-    jimpClient.setMargin(2)
-    const title = await flickrClient.getImageTitle() + ' » ' + process.env.CUSTOM_TEXT + ' « ' + flickrImageId
-    await jimpClient.setTextBox(title, 8, 256, 64, 0)
-
-    // 3. saving the modified photo
-    await jimpClient.saveProcessedImage('./data/processed', `${flickrImageId}.jpg`)
+    for (const photoId of flickrAlbumImageIds) {
+        await jimpClient.setPhoto(`./data/original/${flickrAlbumId}/${photoId}.jpg`)
+        jimpClient.setAspectRatio(1.5)
+        const title = await flickrClient.getImageTitle(photoId) + ' » ' + process.env.CUSTOM_TEXT + ' « ' + photoId
+        await jimpClient.setTextBox(title, 8, 256, 64, 0)
+        jimpClient.setMargin(2)
+        await jimpClient.saveProcessedImage(`./data/processed/${flickrAlbumId}`, `${photoId}.jpg`)
+        jimpClient.resetCanvas()
+    }
 })()
