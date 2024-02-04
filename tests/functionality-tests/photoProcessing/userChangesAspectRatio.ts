2e226559dcdc057577e777f586f70d94ae7dae68
dgerike <dennis.gerike+github@gmail.com>
2024-02-04 11:24:50 +0100

    adding a functionality test to check different aspect ratios

---
+++ tests/functionality-tests/photoProcessing/userChangesAspectRatio.ts
@@ -0,0 +1,35 @@
+import {JimpClient} from "../../../lib/jimp/JimpClient"
+import {getProcessedFolderPath} from "../_helper/getProcessedFolderPath"
+import sizeOf from "image-size"
+import assert from "assert"
+
+describe.each([
+    3 / 2,
+    1,
+    16 / 9,
+])
+('aspect ratio', (aspectRatio: number) => {
+    test(`the final photo should have an aspect ratio of ${aspectRatio}`, async () => {
+        // create image
+        const jimpClient = new JimpClient()
+        jimpClient.setAspectRatio(aspectRatio)
+        await jimpClient.saveProcessedImage(getProcessedFolderPath(), `aspect_ratio_${aspectRatio}.jpg`)
+
+        // fetch dimensions
+        const dimensions = sizeOf(`${getProcessedFolderPath()}/aspect_ratio_${aspectRatio}.jpg`)
+        const width = dimensions.width
+        const height = dimensions.height
+
+        // check aspect ratio
+        if (width && height) {
+            assertAspectRatio(width, height, aspectRatio)
+        } else {
+            assert(false)
+        }
+    })
+})
+
+function assertAspectRatio(width: number, height: number, expectedAspectRatio: number, epsilon: number = 0.001) {
+    const actualAspectRatio = width / height
+    assert(Math.abs(expectedAspectRatio - actualAspectRatio) < epsilon)
+}
