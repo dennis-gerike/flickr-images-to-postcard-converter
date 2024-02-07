97bf3c5a95d1d54367c7ba68979a51521ea4e808
dgerike <dennis.gerike+github@gmail.com>
2024-02-07 14:57:24 +0100

    FP2PC-19 enabling the user to change the download directory

--- lib/converter/getDownloadFolderPath.ts
+++ lib/converter/getDownloadFolderPath.ts
@@ -1,5 +1,14 @@
-import {getAlbumId} from "./getAlbumId"
+import {getAlbumId} from "./getAlbumId";
 
+/**
+ * A folder is needed to store the downloaded images.
+ * Either the user specifies a path or we do.
+ */
 export function getDownloadFolderPath() {
+    const path = process.env.DOWNLOAD_PATH
+    if (path) {
+        return path
+    }
+
     return `${__dirname}/../../data/original/${getAlbumId()}`
 }
