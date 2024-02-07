c183cd0e80b3b7508c14b5f9d9650e6a19d3e7c5
dgerike <dennis.gerike+github@gmail.com>
2024-02-07 15:00:30 +0100

    FP2PC-20 enabling the user to change the processed directory

--- lib/converter/getProcessedFolderPath.ts
+++ lib/converter/getProcessedFolderPath.ts
@@ -1,5 +1,14 @@
 import {getAlbumId} from "./getAlbumId"
 
+/**
+ * A folder is needed to store the processed images.
+ * Either the user specifies a path or we do.
+ */
 export function getProcessedFolderPath() {
+    const path = process.env.PROCESSED_PATH
+    if (path) {
+        return path
+    }
+
     return `${__dirname}/../../data/processed/${getAlbumId()}`
 }
