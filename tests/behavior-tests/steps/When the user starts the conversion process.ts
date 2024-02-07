d104a33fa1273138175559332f447497e3c87702
dgerike <dennis.gerike+github@gmail.com>
2024-02-07 17:47:22 +0100

    when the app fails in any way we have to remember this information for the following test steps

--- tests/behavior-tests/steps/When the user starts the conversion process.ts
+++ tests/behavior-tests/steps/When the user starts the conversion process.ts
@@ -2,5 +2,9 @@ import {When} from "@cucumber/cucumber"
 import {execSync} from "child_process"
 
 When('the user starts the conversion process', async function () {
-    execSync('npm start')
+    try {
+        execSync('npm start')
+    } catch (error) {
+        this.conversionProcessFailed = true
+    }
 })
