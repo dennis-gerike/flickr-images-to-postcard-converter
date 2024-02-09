8b75d4e175159526b2c9845f363144d39b0fbf63
dgerike <dennis.gerike+github@gmail.com>
2024-02-09 13:23:51 +0100

    FP2PC-21 adding tests

---
+++ tests/functionality-tests/downloadDirectory/userSpecifiesCustomDownloadDirectory.ts
@@ -0,0 +1,29 @@
+import fs from "fs"
+import assert from "assert"
+import {getMockedFlickrApiClient} from "../_helper/mockFlickApi"
+import {FlickrClient} from "../../../lib/flickr/FlickrClient"
+import {getDownloadFolderPath} from "../_helper/getDownloadFolderPath"
+
+test('when the user specified a download directory, then this should be used for the photos', async () => {
+    const flickrClient = new FlickrClient('DUMMY_API_KEY', getMockedFlickrApiClient())
+    const randomNumber = Math.floor(Math.random() * 10000)
+    const targetPath = `${getDownloadFolderPath()}/${randomNumber}`
+
+    const targetFile = 'dummy.jpg'
+    await flickrClient.downloadOriginalImage("DUMMY_ID", targetPath, targetFile)
+    const fullPath = `${targetPath}/${targetFile}`
+    const fileExists = fs.existsSync(fullPath)
+    assert(fileExists, `File "${fullPath}" not found`)
+})
+
+test('when the user specified a download directory, then this should be used for the meta information', async () => {
+    const flickrClient = new FlickrClient('DUMMY_API_KEY', getMockedFlickrApiClient())
+    const randomNumber = Math.floor(Math.random() * 10000)
+    const targetPath = `${getDownloadFolderPath()}/${randomNumber}`
+
+    const targetFile = 'dummy.json'
+    await flickrClient.downloadImageInformation("DUMMY_ID", targetPath, targetFile)
+    const fullPath = `${targetPath}/${targetFile}`
+    const fileExists = fs.existsSync(fullPath)
+    assert(fileExists, `File "${fullPath}" not found`)
+})
