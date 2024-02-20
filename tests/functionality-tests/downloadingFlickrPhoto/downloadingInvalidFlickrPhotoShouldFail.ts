46b304ed6339e0cc4ac07f1e4bdc92470f6e0b80
dgerike <dennis.gerike+github@gmail.com>
2024-02-20 12:00:47 +0100

    FP2PC-31 updating test

--- tests/functionality-tests/downloadingFlickrPhoto/downloadingInvalidFlickrPhotoShouldFail.ts
+++ tests/functionality-tests/downloadingFlickrPhoto/downloadingInvalidFlickrPhotoShouldFail.ts
@@ -3,7 +3,7 @@ import {FlickrClient} from "../../../lib/flickr/FlickrClient"
 import {getDownloadFolderPath} from "../_helper/getDownloadFolderPath"
 import {getMockedFlickrApiClient, TestSituation} from "../_helper/getMockedFlickrApiClient"
 
-test('user selects a Flickr photo that does not exist', async () => {
+test('downloading a Flickr photo that does not exist should fail', async () => {
     const invalidFlickrId = '-5'
     const flickrClient = new FlickrClient('DUMMY_API_KEY', getMockedFlickrApiClient(TestSituation.failure))
 
