355cd8d189c0d999bb8bb7c123a27c7fae265b02
dgerike <dennis.gerike+github@gmail.com>
2024-03-04 11:04:02 +0100

    FP2PC-44 async not needed here

--- tests/functionality-tests/addingCaption/placeholderReplacement/usingPlaceholdersInCaption.ts
+++ tests/functionality-tests/addingCaption/placeholderReplacement/usingPlaceholdersInCaption.ts
@@ -1,13 +1,13 @@
 import {resolvePlaceholdersInCaption} from "../../../../lib/converter/resolvePlaceholdersInCaption"
 
-test("when no placeholders were specified, then the caption should not change", async () => {
+test("when no placeholders were specified, then the caption should not change", () => {
     const caption = "Test 1234"
     const resolvedCaption = resolvePlaceholdersInCaption(caption)
 
     expect(resolvedCaption).toEqual(caption)
 })
 
-test("when an unknown placeholder was specified, then the caption should not change", async () => {
+test("when an unknown placeholder was specified, then the caption should not change", () => {
     const caption = "Test 1234 <BLUBB>"
     const resolvedCaption = resolvePlaceholdersInCaption(caption)
 
