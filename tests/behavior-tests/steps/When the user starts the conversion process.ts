457d6c45a7c1b1e75328d19e8aa5fb71e52eb068
dgerike <dennis.gerike+github@gmail.com>
2024-03-04 15:35:34 +0100

    async modifier not necessary here

--- tests/behavior-tests/steps/When the user starts the conversion process.ts
+++ tests/behavior-tests/steps/When the user starts the conversion process.ts
@@ -1,7 +1,7 @@
 import {When} from "@cucumber/cucumber"
 import {execSync} from "child_process"
 
-When('the user starts the conversion process', async function () {
+When('the user starts the conversion process', function () {
     try {
         execSync('npm start')
     } catch (error) {
