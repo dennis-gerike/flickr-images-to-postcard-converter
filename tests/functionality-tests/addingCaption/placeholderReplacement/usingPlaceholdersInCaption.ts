1c4ec2eb5d13e8b57fcd7194bbd46ccf93a1e0a9
dgerike <dennis.gerike+github@gmail.com>
2024-03-04 10:10:50 +0100

    FP2PC-44 adding test

--- tests/functionality-tests/addingCaption/placeholderReplacement/usingPlaceholdersInCaption.ts
+++ tests/functionality-tests/addingCaption/placeholderReplacement/usingPlaceholdersInCaption.ts
@@ -6,3 +6,10 @@ test("when no placeholders were specified, then the caption should not change", 
 
     expect(resolvedCaption).toEqual(caption)
 })
+
+test("when an unknown placeholder was specified, then the caption should not change", async () => {
+    const caption = "Test 1234 <BLUBB>"
+    const resolvedCaption = resolvePlaceholdersInCaption(caption)
+
+    expect(resolvedCaption).toEqual(caption)
+})
