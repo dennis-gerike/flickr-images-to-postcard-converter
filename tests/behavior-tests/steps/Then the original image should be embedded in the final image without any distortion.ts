f178861353bdbc80b5b841f07d17c0643a859abe
dgerike <dennis.gerike+github@gmail.com>
2024-03-04 15:43:58 +0100

    FP2PC-41 adding a separate fixture

--- tests/behavior-tests/steps/Then the original image should be embedded in the final image without any distortion.ts
+++ tests/behavior-tests/steps/Then the original image should be embedded in the final image without any distortion.ts
@@ -4,7 +4,7 @@ import {getFailuresFolderPath} from "../_helper/getFailuresFolderPath"
 import {assertImagesAreTheSame} from "../_helper/assertImagesAreTheSame"
 
 Then('the original image should be embedded in the final image without any distortion', {timeout: 10000}, async function () {
-    const referenceImagePath = `${getFixturesFolderPath()}/reference-images/${process.env.FLICKR_IMAGE_ID}.jpg`
+    const referenceImagePath = `${getFixturesFolderPath()}/reference-images/${process.env.FLICKR_IMAGE_ID}-${process.env.ASPECT_RATIO}.jpg`
     const processedImagePath = `${__dirname}/../../../data/processed/null/${process.env.FLICKR_IMAGE_ID}.jpg`
     const diffImagePath = `${getFailuresFolderPath()}/${process.env.FLICKR_IMAGE_ID}_diff.png`
 
