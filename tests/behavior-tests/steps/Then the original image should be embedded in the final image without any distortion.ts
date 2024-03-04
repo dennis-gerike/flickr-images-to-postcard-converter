b9a504955a7684a8faa9be603f70d0d83e62227b
dgerike <dennis.gerike+github@gmail.com>
2024-03-04 15:35:51 +0100

    reducing code duplication

--- tests/behavior-tests/steps/Then the original image should be embedded in the final image without any distortion.ts
+++ tests/behavior-tests/steps/Then the original image should be embedded in the final image without any distortion.ts
@@ -1,26 +1,12 @@
 import {Then} from "@cucumber/cucumber"
-import assert from "assert"
-import looksSame from "looks-same"
 import {getFixturesFolderPath} from "../_helper/getFixturesFolderPath"
 import {getFailuresFolderPath} from "../_helper/getFailuresFolderPath"
-import {createFailuresFolderPath} from "../_helper/createFailuresFolderPath"
+import {assertImagesAreTheSame} from "../_helper/assertImagesAreTheSame"
 
 Then('the original image should be embedded in the final image without any distortion', {timeout: 10000}, async function () {
     const referenceImagePath = `${getFixturesFolderPath()}/reference-images/${process.env.FLICKR_IMAGE_ID}.jpg`
     const processedImagePath = `${__dirname}/../../../data/processed/null/${process.env.FLICKR_IMAGE_ID}.jpg`
     const diffImagePath = `${getFailuresFolderPath()}/${process.env.FLICKR_IMAGE_ID}_diff.png`
-    const {
-        equal,
-        diffImage,
-        differentPixels,
-        totalPixels
-    } = await looksSame(referenceImagePath, processedImagePath, {createDiffImage: true})
 
-    if (!equal) {
-        await createFailuresFolderPath()
-        await diffImage.save(diffImagePath)
-        console.log(`${differentPixels} out of ${totalPixels} pixels are different.`)
-    }
-
-    assert.equal(equal, true)
+    await assertImagesAreTheSame(referenceImagePath, processedImagePath, diffImagePath)
 })
