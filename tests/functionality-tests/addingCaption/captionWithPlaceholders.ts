6078559c04708309c583b6e8538fedd83b4a1f87
dgerike <dennis.gerike+github@gmail.com>
2024-04-24 16:46:00 +0200

    updating tests

--- tests/functionality-tests/addingCaption/captionWithPlaceholders.ts
+++ tests/functionality-tests/addingCaption/captionWithPlaceholders.ts
@@ -1,26 +1,26 @@
-import {resolvePlaceholdersInCaption} from "../../../../lib/converter/resolvePlaceholdersInCaption"
-import {getFixturesFolderPath} from "../../_helper/getFixturesFolderPath"
-import {ImageInformation} from "../../../../lib/flickr/types/internal/ImageInformation"
+import {resolvePlaceholdersInCaption} from "../../../lib/converter/resolvePlaceholdersInCaption"
+import {getFixturesFolderPath} from "../_helper/getFixturesFolderPath"
+import {ImageInformation} from "../../../lib/flickr/types/internal/ImageInformation"
 
 function getDummyImageInformation(): ImageInformation {
     return {id: "", title: "", url: ""}
 }
 
-test("when no placeholders were specified, then the caption should not change", () => {
+test("there should not happen any replacements when no placeholder was specified", () => {
     const caption = "Test 1234"
     const resolvedCaption = resolvePlaceholdersInCaption(caption, getDummyImageInformation())
 
     expect(resolvedCaption).toEqual(caption)
 })
 
-test("when an unknown placeholder was specified, then the caption should not change", () => {
+test("unknown placeholders should be ignored and not be replaced", () => {
     const caption = "Test 1234 <BLUBB>"
     const resolvedCaption = resolvePlaceholdersInCaption(caption, getDummyImageInformation())
 
     expect(resolvedCaption).toEqual(caption)
 })
 
-test("when the placeholder <PHOTO_ID> was specified in the caption, then it should be resolved", () => {
+test("the placeholder PHOTO_ID should be resolved", () => {
     const flickrId = "123456789"
     let imageInformation = getDummyImageInformation()
     imageInformation.id = flickrId
@@ -32,12 +32,24 @@ test("when the placeholder <PHOTO_ID> was specified in the caption, then it shou
     expect(resolvedCaption).toEqual(expectedCaption)
 })
 
-test("when the placeholder <PHOTO_TITLE> was specified in the caption, then it should be resolved", () => {
+test("the placeholder PHOTO_TITLE should be resolved", () => {
     const imageInformation: ImageInformation = require(`${getFixturesFolderPath()}/16by9.json`)
 
     const caption = "Test 1234 <PHOTO_TITLE>"
     const resolvedCaption = resolvePlaceholdersInCaption(caption, imageInformation)
     const expectedCaption = `Test 1234 ${imageInformation.title}`
+
+    expect(resolvedCaption).toEqual(expectedCaption)
+})
+
+test("multiple placeholders should be resolved", () => {
+    const flickrId = "123456789"
+    let imageInformation = getDummyImageInformation()
+    imageInformation.id = flickrId
+
+    const caption = "Test - <PHOTO_ID> - <PHOTO_TITLE>"
+    const resolvedCaption = resolvePlaceholdersInCaption(caption, imageInformation)
+    const expectedCaption = `Test - ${flickrId} - ${imageInformation.title}`
 
     expect(resolvedCaption).toEqual(expectedCaption)
 })
