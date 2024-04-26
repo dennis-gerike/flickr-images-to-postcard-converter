f517b010d8803affa26852ffb13425de17efb591
dgerike <dennis.gerike+github@gmail.com>
2024-04-26 12:17:31 +0200

    this test steps also needs to set the aspect ratio

--- tests/behavior-tests/steps/When the user selects a photo with an aspect ratio of {int}-{int}.ts
+++ tests/behavior-tests/steps/When the user selects a photo with an aspect ratio of {int}-{int}.ts
@@ -5,4 +5,5 @@ import {EnvironmentVariables} from "../../../lib/converter/types/EnvironmentVari
 When('the user selects a photo with an aspect ratio of {int}:{int}', function (x: number, y: number) {
     const fixture = require(`${getFixturesFolderPath()}/${x}by${y}`)
     process.env[EnvironmentVariables.FLICKR_IMAGE_ID] = fixture.id
+    process.env[EnvironmentVariables.ASPECT_RATIO] = `${x / y}`
 })
