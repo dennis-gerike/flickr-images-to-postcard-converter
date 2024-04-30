46631fa16bf10f4a124469c3e4a86eb8ad067998
dgerike <dennis.gerike+github@gmail.com>
2024-04-30 14:56:14 +0200

    FP2PC-61 testing multiple invalid buffer sizes

--- tests/functionality-tests/buffer/invalidBufferSize.ts
+++ tests/functionality-tests/buffer/invalidBufferSize.ts
@@ -1,21 +1,27 @@
 import {JimpClient} from "../../../lib/jimp/JimpClient"
 import {getFixturesFolderPath} from "../_helper/getFixturesFolderPath"
 
-test('using invalid buffer sizes should lead to an error', async () => {
-    const bufferSize = -1
+describe.each([
+    -1,
+    -0.1,
+    100.01,
+    101,
+])
+('buffer', (bufferSize: number) => {
+    test(`a buffer size of ${bufferSize} should lead to an error`, () => {
+        // selecting a fixture as input image
+        const source = `${getFixturesFolderPath()}/1by1`
+        const sourcePhotoPath = `${source}_medium.jpg`
+        const photoId = require(`${source}.json`).id
 
-    // selecting a fixture as input image
-    const source = `${getFixturesFolderPath()}/1by1`
-    const sourcePhotoPath = `${source}_medium.jpg`
-    const photoId = require(`${source}.json`).id
-
-    // trying to set invalid buffer
-    const jimpClient = new JimpClient()
-    expect(() => {
-        jimpClient.setCaption({
-            relativeVerticalBuffer: bufferSize,
-            relativeHeight: 5,
-            text: "dummy text"
-        })
-    }).toThrow(Error)
+        // trying to set invalid buffer
+        const jimpClient = new JimpClient()
+        expect(() => {
+            jimpClient.setCaption({
+                relativeVerticalBuffer: bufferSize,
+                relativeHeight: 5,
+                text: "dummy text"
+            })
+        }).toThrow(Error)
+    })
 })
