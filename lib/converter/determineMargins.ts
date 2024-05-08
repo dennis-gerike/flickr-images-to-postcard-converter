0c486e1db8e0db1481b80be59e38f59a9d0e9afb
dgerike <dennis.gerike+github@gmail.com>
2024-05-08 10:49:44 +0200

    FP2PC-80 restricting the range of valid margin values

--- lib/converter/determineMargins.ts
+++ lib/converter/determineMargins.ts
@@ -1,10 +1,19 @@
-export function determineMargin(givenMargin: string | undefined, defaultMargin: number = 0): number {
+export function determineMargin(
+    givenMargin: string | undefined,
+    defaultMargin: number = 0,
+    minValue: number = 0,
+    maxValue: number = 100
+): number {
     if (typeof givenMargin === "undefined" || givenMargin === "") {
         return defaultMargin
     }
 
     if (isNaN(Number(givenMargin))) {
         throw new Error('Invalid margin value provided!')
+    }
+
+    if (Number(givenMargin) < minValue || Number(givenMargin) > maxValue) {
+        throw new Error('Margin size out of range!')
     }
 
     return Number(givenMargin)
