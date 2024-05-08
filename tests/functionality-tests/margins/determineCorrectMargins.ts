00e501644b1715d540732a556335cd57006db06a
dgerike <dennis.gerike+github@gmail.com>
2024-05-08 10:50:04 +0200

    FP2PC-80 adding tests

--- tests/functionality-tests/margins/determineCorrectMargins.ts
+++ tests/functionality-tests/margins/determineCorrectMargins.ts
@@ -27,3 +27,49 @@ test('when the margin is empty then the default value should be determined', asy
     expect(determinedVerticalMargin)
         .toEqual(0)
 })
+
+test('when the margin is invalid then an error should occur', async () => {
+    process.env[EnvironmentVariables.MARGIN_HORIZONTAL] = "seven"
+    expect(() => {
+        determineHorizontalMargin()
+    }).toThrow(Error)
+
+    process.env[EnvironmentVariables.MARGIN_VERTICAL] = "eight"
+    expect(() => {
+        determineVerticalMargin()
+    }).toThrow(Error)
+})
+
+test('when the margin is out of range then an error should occur', async () => {
+    process.env[EnvironmentVariables.MARGIN_HORIZONTAL] = "101"
+    expect(() => {
+        determineHorizontalMargin()
+    }).toThrow(Error)
+
+    process.env[EnvironmentVariables.MARGIN_VERTICAL] = "-5"
+    expect(() => {
+        determineVerticalMargin()
+    }).toThrow(Error)
+})
+
+test('when the margin is valid then no error should occur', async () => {
+    process.env[EnvironmentVariables.MARGIN_HORIZONTAL] = "11"
+    expect(() => {
+        determineHorizontalMargin()
+    }).not.toThrow(Error)
+
+    process.env[EnvironmentVariables.MARGIN_VERTICAL] = "17"
+    expect(() => {
+        determineVerticalMargin()
+    }).not.toThrow(Error)
+})
+
+test('a valid margin should be determined correctly', async () => {
+    process.env[EnvironmentVariables.MARGIN_HORIZONTAL] = "3"
+    expect(determineHorizontalMargin())
+        .toEqual(3)
+
+    process.env[EnvironmentVariables.MARGIN_VERTICAL] = "9"
+    expect(determineVerticalMargin())
+        .toEqual(9)
+})
