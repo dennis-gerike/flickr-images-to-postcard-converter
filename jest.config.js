0e11d921b5f710141a0c6a30fb51af45c4e64d7b
dgerike <dennis.gerike+github@gmail.com>
2024-05-10 12:49:20 +0200

    updating the format of the junit output

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,12 @@ module.exports = {
     ],
     reporters: [
         'default',
-        ['jest-junit', {"outputDirectory": "test-reports"}],
+        ['jest-junit', {
+            "outputDirectory": "test-reports",
+            "classNameTemplate": "{classname}",
+            "titleTemplate": "{title}",
+            "ancestorSeparator": " » "
+        }],
         'github-actions'
     ],
     collectCoverageFrom: ['lib/**/*.ts'],
