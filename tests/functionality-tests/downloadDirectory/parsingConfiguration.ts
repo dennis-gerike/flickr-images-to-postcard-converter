95b27f791859002c3c9d28a53415e3060b2ac87f
dgerike <dennis.gerike+github@gmail.com>
2024-05-13 12:21:19 +0200

    refactoring all "download directory" tests

--- tests/functionality-tests/downloadDirectory/parsingConfiguration.ts
+++ tests/functionality-tests/downloadDirectory/parsingConfiguration.ts
@@ -8,35 +8,39 @@ import path from "node:path"
 describe('Download Directory', () => {
     describe('Parsing configuration', () => {
         test('When no folder was configured then the default path should be determined', () => {
+            const mediaId = "DUMMY_ID"
             delete process.env[EnvironmentVariables.DOWNLOAD_PATH]
-            const determinedFolderPath = determineDownloadFolderPath()
+            const determinedFolderPath = determineDownloadFolderPath(mediaId)
 
             expect(determinedFolderPath)
-                .toEqual(path.resolve(`${__dirname}/../../../data/original/null`))
+                .toEqual(path.resolve(`${__dirname}/../../../data/original/${mediaId}`))
         })
 
         test('When an empty folder name was provided then the default path should be determined', () => {
+            const mediaId = "DUMMY_ID"
             process.env[EnvironmentVariables.DOWNLOAD_PATH] = ""
-            const determinedFolderPath = determineDownloadFolderPath()
+            const determinedFolderPath = determineDownloadFolderPath(mediaId)
 
             expect(determinedFolderPath)
-                .toEqual(path.resolve(`${__dirname}/../../../data/original/null`))
+                .toEqual(path.resolve(`${__dirname}/../../../data/original/${mediaId}`))
         })
 
         test('A valid folder name should be determined correctly', () => {
+            const mediaId = "DUMMY_ID"
             process.env[EnvironmentVariables.DOWNLOAD_PATH] = "/home/test/tmp/flickr/albums/"
-            const determinedFolderPath = determineDownloadFolderPath()
+            const determinedFolderPath = determineDownloadFolderPath(mediaId)
 
             expect(determinedFolderPath)
-                .toEqual("/home/test/tmp/flickr/albums")
+                .toEqual(`/home/test/tmp/flickr/albums/${mediaId}`)
         })
 
         test('Relative folder paths should be resolved correctly', () => {
+            const mediaId = "DUMMY_ID"
             process.env[EnvironmentVariables.DOWNLOAD_PATH] = "/home/test/tmp/flickr/misc/../albums/"
-            const determinedFolderPath = determineDownloadFolderPath()
+            const determinedFolderPath = determineDownloadFolderPath(mediaId)
 
             expect(determinedFolderPath)
-                .toEqual("/home/test/tmp/flickr/albums")
+                .toEqual(`/home/test/tmp/flickr/albums/${mediaId}`)
         })
     })
 })
